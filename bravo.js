8cb615546fb4e4b22366341b9c8e7bf7062d6657
thatonepuggo <thatonepuggo@gmail.com>
2022-07-01 20:42:32 -0400

    made all windows modal on bc i cant fix where windows in winbox will stay at the top

--- bravo.js
+++ bravo.js
@@ -1,11 +1,14 @@
 const cb = document.querySelector('#admin');
 
-function app(link) {
+function app(link, name) {
     var postfix = '';
     if (cb.checked) postfix = '/#admin';
-    var winBox = new WinBox('Terminal', {
+    var winBox = new WinBox({
+        title: name,
         url: link + postfix,
-//        modal: true,
+        modal: true,
+        x: "center",
+        y: "50%",
         width: 600,
         height: 500,
     });
